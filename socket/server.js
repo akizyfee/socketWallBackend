ef5f77fa48eda2400e67508eddb07d26351b5a9d
unknown <akidyfee@gmail.comgit config --global user.name akizyfeegit config --global user.email akidyfee@gmail.com>
2022-05-19 13:39:15 +0800

    fix

--- socket/server.js
+++ socket/server.js
@@ -1,11 +1,9 @@
 
 const socketall = (socket) => {
     socket.on('chat message', (msg) => {
-        console.log(msg);
         socket.broadcast.emit('chat message', msg);
       });
-      socket.on('imgFileBase64', (img) => {
-        // console.log(img);終端機會很壞的瘋狂洗版 先註解掉
+    socket.on('imgFileBase64', (img) => {
         socket.broadcast.emit('imgFileBase64', img);
     });
 };
