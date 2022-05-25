a549c728819e0984f66fe792d05f4552a4c7c3ef
akizyfee <87452790+akizyfee@users.noreply.github.com>
2022-05-25 21:46:32 +0800

    fix img

--- controllers/addImg.js
+++ controllers/addImg.js
@@ -1,11 +1,11 @@
-const addImgs = require('../models/addImgsModel');
+const addImg = require('../models/addImgsModel');
 const handleSuccess = require('../service/handleSuccess');
 const handleErrorAsync = require('../service/handleErrorAsync');
 
 
 const addImgs = {
     getImgs: handleErrorAsync(async (req, res) => {
-        const addImgs = await addImgs.find()
+        const addImgs = await addImg.find()
         handleSuccess(res, '資料讀取成功', addImgs);
     }),
 }
